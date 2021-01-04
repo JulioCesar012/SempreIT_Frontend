2559711a024470db145d0db5036e8719f49918d8
Julio Filho <rodriguesjuliocesar145@gmail.com>
2021-01-04 00:46:21 -0300

    Update index.js

--- src/components/SidebarAdmin/index.js
+++ src/components/SidebarAdmin/index.js
@@ -41,7 +41,7 @@ function SidebarAdmin() {
       </Menu.Item>
 
       <Menu.Item key="logout" className="button_menu">
-        <Link to="/logout" onClick={handleLogout} >
+        <Link to="/" onClick={handleLogout} >
           <AntIcons.LogoutOutlined />
           Logout
         </Link>
